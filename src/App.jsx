e364904d82a56eb0855239f4b79a6a967029901a
IDGS-1004-19002243 <19002243@alumnos.utleon.edu.mx>
2026-02-10 09:13:45 -0600

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Login from './paginas/Login';
 import LayoutAdmin from './componentes/LayoutAdmin';
 import Dashboard from './paginas/Dashboard';
 import EditarInicio from './paginas/EditarInicio';
-import EditarNosotros from './paginas/EditarNosotros';
-import EditarContacto from './paginas/EditarContacto';
+// import EditarNosotros from './paginas/EditarNosotros'; // Enlace a Nosotros comentado
+// import EditarContacto from './paginas/EditarContacto'; // Enlace a Contacto comentado
 import CatalogoHombre from './paginas/CatalogoHombre';
 import CatalogoMujer from './paginas/CatalogoMujer';
 import EditarResponsabilidad from './paginas/EditarResponsabilidad';
-import EditarDistribuidores from './paginas/EditarDistribuidores';
-// import Mensajes from './paginas/Mensajes'; // Acceso a Mensajes comentado
+// import EditarDistribuidores from './paginas/EditarDistribuidores'; // Enlace a Distribuidores comentado
+// import Mensajes from './paginas/Mensajes'; 
 import Configuracion from './paginas/Configuracion';
 
 // Componente para proteger rutas
@@ -43,13 +43,15 @@ function App() {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="editar-inicio" element={<EditarInicio />} />
           
-          <Route path="editar-nosotros" element={<EditarNosotros />} />
-          <Route path="editar-contacto" element={<EditarContacto />} />
+          {/* Enlaces comentados: Nosotros, Contacto y Distribuidores */}
+          {/* <Route path="editar-nosotros" element={<EditarNosotros />} /> */}
+          {/* <Route path="editar-contacto" element={<EditarContacto />} /> */}
           
           <Route path="editar-catalogo-hombre" element={<CatalogoHombre />} />
           <Route path="editar-catalogo-mujer" element={<CatalogoMujer />} />
           <Route path="editar-responsabilidad" element={<EditarResponsabilidad />} />
-          <Route path="editar-distribuidores" element={<EditarDistribuidores />} />
+          
+          {/* <Route path="editar-distribuidores" element={<EditarDistribuidores />} /> */}
           
           {/* Ruta de mensajes comentada */}
           {/* <Route path="mensajes" element={<Mensajes />} /> */}
